c6ecbfd020fd3e7c82d11ff983086b2fea2adb4c
Raul Santos <raul.26.santos@gmail.com>
2024-03-31 21:11:06 -0300

    criado a entidade marca_produto

---
+++ lojaVirtual/src/main/java/br/com/rps/lojaVirtual/models/MarcaProduto.java
@@ -0,0 +1,22 @@
+package br.com.rps.lojaVirtual.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "marca_produto")
+@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
+@Data
+public class MarcaProduto implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+    private Long id;
+    private String NomeDesc;
+
+}
